5b21de16fc2c856e9a4226cce6d404d9512a7a8b
German Sanchez <Luqh555@gmail.com>
2021-04-25 14:34:32 +0200

    Encuadrando el texto dentro del papiro principal del body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { PapiroComponent } from './papiro/papiro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PapiroComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/papiro/papiro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-papiro',
+  templateUrl: './papiro.component.html',
+  styleUrls: ['./papiro.component.css']
+})
+export class PapiroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
