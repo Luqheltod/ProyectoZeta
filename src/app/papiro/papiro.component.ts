294a2ea71b6c5322f78056d7216e05cc2a46a542
German Sanchez <Luqh555@gmail.com>
2021-05-30 17:33:43 +0200

    Simulamos a papiro como si viniese de un rest de spring

--- src/app/papiro/papiro.component.ts
+++ src/app/papiro/papiro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Papiro } from './papiro';
-
+import {PAPIRO} from './papiro.json';
 @Component({
   selector: 'app-papiro',
   templateUrl: './papiro.component.html',
@@ -8,13 +8,12 @@ import { Papiro } from './papiro';
 })
 export class PapiroComponent implements OnInit {
 
-  papiro: Papiro =
-  {
-    id:1, texto:"El texto uno que sea",idopciones:1,idmapa:1 
-  };
+  papiro: Papiro  ;
+ 
   constructor() { }
 
   ngOnInit(): void {
+    this.papiro=  PAPIRO;
   }
 
 }
---
+++ src/app/papiro/papiro.json.ts
@@ -0,0 +1,6 @@
+import {Papiro} from './papiro';
+
+export const PAPIRO:Papiro =
+{
+    id:1, texto:'El texto uno que sea',idopciones:1,idmapa:1 
+  };
