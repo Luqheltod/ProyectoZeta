f535575af1d088391eb24cba8285751bf68091c7
German Sanchez <Luqh555@gmail.com>
2021-05-30 22:13:54 +0200

    Servicios de mapa y opciones

---
+++ src/app/opciones/opciones.ts
@@ -0,0 +1,13 @@
+
+export class Opciones {
+
+    id: number;
+    opcionuno:string;
+    opciondos:string;
+    opciontres:string;
+    opcioncuatro:string;
+    idopcionuno:number;
+    idopciondos:number;
+    idopciontres:number;
+    idopcioncuatro:number;
+}
